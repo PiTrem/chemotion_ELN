4a1fbc6e81b3d5d2ffb5487bb32943506eb1daf2
Tasnim Mehzabin <tasnim.mehzabin@kit.edu>
2023-03-07 15:43:59 +0100

    fix rspec and rubocop warnings

--- db/schema.rb
+++ db/schema.rb
@@ -229,7 +229,6 @@ ActiveRecord::Schema.define(version: 2023_05_03_090936) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "parent_id"
-    t.integer "attachments_count"
     t.index ["containable_type", "containable_id"], name: "index_containers_on_containable"
   end
 
@@ -1334,6 +1333,7 @@ ActiveRecord::Schema.define(version: 2023_05_03_090936) do
             )
             and sync_cols.user_id in (select user_ids(in_user_id))
         ) all_cols;
+
           return query select coalesce(i_detail_level_sample,0) detail_level_sample, coalesce(i_detail_level_wellplate,0) detail_level_wellplate;
       end;$function$
   SQL
@@ -1359,6 +1359,7 @@ ActiveRecord::Schema.define(version: 2023_05_03_090936) do
       begin
       	select channel_type into i_channel_type
       	from channels where id = in_channel_id;
+
         case i_channel_type
       	when 9 then
       	  insert into notifications (message_id, user_id, created_at,updated_at)
@@ -1436,6 +1437,7 @@ ActiveRecord::Schema.define(version: 2023_05_03_090936) do
       	if (TG_OP='INSERT') then
           PERFORM generate_users_matrix(null);
       	end if;
+
       	if (TG_OP='UPDATE') then
       	  if new.enabled <> old.enabled or new.deleted_at <> new.deleted_at then
             PERFORM generate_users_matrix(null);
